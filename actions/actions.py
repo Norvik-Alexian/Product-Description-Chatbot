76a62261748d9244202ec278d3bc5d3acc2093a4
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-10-08 19:40:43 +0400

    create a custom action that generates product description using gpt-3 Model

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,44 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import os
+import requests
+
+from typing import Any, Text, Dict, List
+from dotenv import load_dotenv
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+load_dotenv()
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionProductDescriptionGenerator(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_product_description"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        PRODUCT_DESCRIPTION_ENDPOINT = os.getenv('PRODUCT_DESCRIPTION_ENDPOINT')
+        UC_X_API_KEY = os.getenv('UC-X-API-KEY')
+
+        headers = {
+            'accept': 'application/json',
+            'UC-X-API-KEY': UC_X_API_KEY,
+        }
+
+        json_data = {
+            'keywords': [
+                'ucraft',
+                'website builder',
+                'ecommerce'
+            ],
+            'content_type': 'description_highlight',
+            'count': 1
+        }
+
+        response = requests.post(PRODUCT_DESCRIPTION_ENDPOINT,
+                                 headers=headers, json=json_data)
+
+        generated_text = '\n'.join(response.json())
+        dispatcher.utter_message(text=generated_text)
+
+        return []
