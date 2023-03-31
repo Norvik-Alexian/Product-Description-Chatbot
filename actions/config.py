dad6f7be477f609ef84c810046b3f93f286d69cf
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-03-31 22:28:50 +0400

    define prompts of the model in config module as constant identifiers

---
+++ actions/config.py
@@ -0,0 +1,12 @@
+SHORT_DESCRIPTION_PROMPT = 'Generate a content that describes the product based on the Targeted Keywords'
+HIGHLIGHTS_PROMPT = 'Generate at least 5 highlights that shows extra information about the product and it is different from actual description'
+DESCRIPTION_PROMPT = '''
+Generate Description: This tactical vest features a sleeveless design, roomy front pockets, and multiple eye-catching metal 
+applique decorations. Put on a pair of cargo pants, and you’re ready for action.
+Highlights:
+* Techwear design
+* Military-style
+* Roomy front pockets
+* Metal applique decorations
+* Solid color.
+'''
